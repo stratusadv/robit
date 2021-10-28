3c1d30226135a26dcdcc9ecac575958cde4feed8
Nathan Johnson <nathanj@stratusadv.com>
2021-10-28 17:28:50 -0600

    Initial

--- demo.py
+++ demo.py
@@ -5,11 +5,16 @@ from time import sleep
 wo = Worker('Test Worker')
 
 
-def test_function():
+def test_function_1():
     sleep(10)
 
 
-wo.add_job('Test Function', test_function)
+def test_function_2():
+    sleep(5)
+
+
+wo.add_job('Test Function 1', test_function_1)
+wo.add_job('Test Function 2', test_function_2)
 
 if __name__ == '__main__':
     wo.run()
