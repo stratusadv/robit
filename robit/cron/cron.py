e19ef44f543502c457a39822fda2a2fc18e57cb8
Wesley Howery <wesleyh@stratusadv.com>
2023-07-17 16:37:45 -0600

    Validation for impossible months (in progress)

--- robit/cron/cron.py
+++ robit/cron/cron.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+import calendar
+
 
 from robit.cron.fields import CronMinuteField, CronHourField, CronDayOfMonthField, CronMonthField, CronDayOfWeekField
-
+from robit.cron.enums import CronFieldTypeEnum
 
 class Cron:
     def __init__(self, cron_string):
@@ -26,7 +28,6 @@ class Cron:
         now = datetime.now()
         next_dt = now.replace(second=0, microsecond=0)
 
-        # Todo: Need to validate it is looking for an actual date or catch the exception.
         while True:
             # Loop through each field and increment the datetime until the next valid datetime is found.
             for key, cron_field in self.field_dict.items():
@@ -41,3 +42,14 @@ class Cron:
                 else:
                     # Increment the minute field and try again.
                     next_dt = self.field_dict['minute'].increment_datetime(next_dt)
+
+    def validate(self):
+        # Todo: Need to validate it is looking for an actual date or catch the exception.
+        month_field = self.field_dict['month']
+        if month_field.type != CronFieldTypeEnum.EVERY:
+            possible_months = month_field.possible_values
+            for month_int in possible_months:
+                #
+                start_date = datetime(year, month, 1)
+                _, last_day = calendar.monthrange(year, month)
+                end_date = datetime(year, month, last_day)
