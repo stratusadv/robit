106b81b61f922d79204c480a827a7cce8187b721
NathanJohnson <nathanj@stratusadv.com>
2023-11-05 00:43:39 -0600

    revert back to setup.cfg

--- robit/config.py
+++ robit/config.py
@@ -1,5 +1,5 @@
 class Config:
-    VERSION: str = 'v0.4.4'
+    VERSION: str = '0.4.4'
     TIMEZONE: str = 'UTC'
     LOG_FILE_NAME: str = 'robit'
     LOG_BACKUP_DAYS: int = 7
--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='robit',
-    version='0.4.4',
-    author='Nathan Johnson & Wesley Howery',
-    author_email='info@stratusadv.com',
-    description='Chronological Automation Service Framework',
-    keywords=['automation', 'bot', 'cron', 'cronjob', 'chronological', 'worker', 'job'],
-    long_description=open('README.md').read(),
-    url='https://github.com/stratusadv/robit',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    license='MIT',
-    include_package_data=True,
-    zip_safe=False,
-    python_requires='>=3.7',
-    install_requires=['pytz'],
-)
+setuptools.setup()
